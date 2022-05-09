b23f76686371efac4127e67e025eb1904da63657
Minh Lượng <66545208+MinhLuong-077@users.noreply.github.com>
2022-05-09 20:45:35 +0700

    Update

--- EWallet/app/src/main/java/com/example/ewallet/HomeFragment.java
+++ EWallet/app/src/main/java/com/example/ewallet/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.ewallet;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
@@ -26,6 +28,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button depositBtn;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -62,6 +66,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        depositBtn = v.findViewById(R.id.deposit);
+        depositBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Deposit.class));
+            }
+        });
+
+        return v;
     }
+
+
+
 }
