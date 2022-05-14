2a8fbb715c2f870c1c8aedf59b17df0bc9fa179a
Hoàng Anh Tuấn <tuanhoang.uit@gmail.com>
2022-05-14 23:12:08 +0700

    create deposit and add toolbar contain back btn inside

--- EWallet/app/src/main/java/com/example/ewallet/Deposit.java
+++ EWallet/app/src/main/java/com/example/ewallet/Deposit.java
@@ -1,8 +1,11 @@
 package com.example.ewallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Deposit extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Deposit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposit);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Deposit");
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Deposit.this, HomeFragment.class));
+            }
+        });
+        setSupportActionBar(toolbar);
     }
 }
